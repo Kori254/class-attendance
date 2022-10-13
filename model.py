67536b3fe5bb06d0fd362f4023cb1e8ac495d019
Lewis Kori <64828228+Kori254@users.noreply.github.com>
2022-10-13 13:51:49 +0300

    Update model.py

--- model.py
+++ model.py
@@ -6,38 +6,39 @@ import cv2
 import tensorflow as tf
 from tensorflow import keras
 from keras.models import load_model
-
+# import trained model from teachable machines.com
 model = load_model('recognizers/keras_model.h5')
 
+#image names are stored as pickle files 
 labels={"person_name":1}
 with open("pickles/face-labels-1.pickle",'rb') as f:
     og_labels=pickle.load(f)
     labels = {v:k for k,v in og_labels.items()}
 
-cap=cv2.VideoCapture('videos/me.mp4')
-
+cap=cv2.VideoCapture()# VideoCapture entails the video that will be used either webcam or video from a directory
+#import mediapipe dependancies
 facedetect=mp.solutions.face_detection
 drawing=mp.solutions.drawing_utils
 face=facedetect.FaceDetection(0.75)
 
-
+#us
 while True:
     ret,frame=cap.read()
-    image=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    image=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #convert to grayscale
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)#convert to RGB
     cv2.namedWindow('recognizer',cv2.WINDOW_NORMAL)    
     #cv2.resizeWindow('recognizer', 560,740)
     #frame=cv2.flip(frame,0)
 
-    results=face.process(gray)
-    my_list=[]
+    results=face.process(gray) #Used to detect face landmarks 
+    
     if results.detections:
         for id,detection in enumerate(results.detections):
             BBOC=detection.location_data.relative_bounding_box
             #print(BBOC)
             ih,iw,ic=frame.shape
             bbox= int(BBOC.xmin*iw),int(BBOC.ymin*ih),\
-                int(BBOC.width*iw),int(BBOC.height*ih)
+                int(BBOC.width*iw),int(BBOC.height*ih) #Vertices of the boundig box 
             cv2.rectangle(frame,bbox,(255,0,255))
             #print(bbox)
             x,y,w,h=bbox[0],bbox[1],bbox[2],bbox[3]
@@ -46,7 +47,7 @@ while True:
             #id_,conf=recognizer.predict(roi)
             img=cv2.resize(img, (224,224))
             img=img.reshape(1, 224, 224, 3)
-            prediction=model.predict(img)
+            prediction=model.predict(img) #compare the bounding box to the trained model
             # for i in prediction:
             #     print(i)
             confidence=prediction[0][1]
